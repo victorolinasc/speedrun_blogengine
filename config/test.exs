30bdfbd8f3f1013d42a4dd648adbc4075192787c
Victor Oliveira Nascimento <victor.olinasc@gmail.com>
2021-04-12 11:48:58 -0300

    chore: tidy up config

--- config/test.exs
+++ config/test.exs
@@ -1,10 +1,5 @@
-use Mix.Config
+import Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
 config :speedrun_blogengine, SpeedrunBlogengine.Repo,
   username: "postgres",
   password: "postgres",
@@ -12,11 +7,8 @@ config :speedrun_blogengine, SpeedrunBlogengine.Repo,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :speedrun_blogengine_web, SpeedrunBlogengineWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn
